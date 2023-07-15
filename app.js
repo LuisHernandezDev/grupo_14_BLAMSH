10d3cb40d045b2a42b87bf25a3bcfa071075863b
lquispe <liliana.quispe@gmail.com>
2023-07-15 11:33:09 -0300

    estructura del proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+// Requerimos express para para usarlo
+const express = require('express');
+const path = require('path');
+const dotenv=require('dotenv').config();
+
+// Iniciamos un servidor, y lo guardamos dentro de app
+const app = express();
+
+app.use(express.static('public'));
+
+
+// Escuchamos los GET request a "/"
+app.get('/', (req, res) => {
+    // Respondemos a este request con este string, mediante res.send
+    const ruta = path.join(__dirname, './views/home.html');
+    res.sendFile(ruta);
+});
+
+
+// Hacemos que nuestro servidor escuche requests en el puerto 3000
+app.listen(process.env.PORT, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
