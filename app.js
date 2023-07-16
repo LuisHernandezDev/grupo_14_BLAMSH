926e9ecd1c9e4ff098eff10d6c418fa6c437a808
lquispe <liliana.quispe@gmail.com>
2023-07-16 17:17:09 -0300

    carrito con header y footer

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ app.use(express.static('public'));
 // Escuchamos los GET request a "/"
 app.get('/', (req, res) => {
     // Respondemos a este request con este string, mediante res.send
-    const ruta = path.join(__dirname, './views/home.html');
+    const ruta = path.join(__dirname, './views/detalleProducto.html');
     res.sendFile(ruta);
 });
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+});
+
 
 
 // Hacemos que nuestro servidor escuche requests en el puerto 3000
