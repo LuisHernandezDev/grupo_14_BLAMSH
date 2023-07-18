89546453f83e8059cd88d9fae9eae28c3c57d207
LuisHernandezDev <lcarlosher@gmail.com>
2023-07-17 21:28:46 -0400

    Detalle de producto html mobile y desktop

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Requerimos express para para usarlo
 const express = require('express');
 const path = require('path');
-const dotenv=require('dotenv').config();
+const dotenv = require('dotenv').config();
 
 // Iniciamos un servidor, y lo guardamos dentro de app
 const app = express();
@@ -12,12 +12,12 @@ app.use(express.static('public'));
 // Escuchamos los GET request a "/"
 app.get('/', (req, res) => {
     // Respondemos a este request con este string, mediante res.send
-    const ruta = path.join(__dirname, './views/home.html');
+    const ruta = path.join(__dirname, './views/detalleProducto.html');
     res.sendFile(ruta);
 });
 
 
 // Hacemos que nuestro servidor escuche requests en el puerto 3000
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
     console.log('Servidor escuchando en el puerto 3000');
 });
