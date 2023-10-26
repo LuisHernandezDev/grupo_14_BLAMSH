64e628353c09c27e3b6d6359f5652bc20db12b6b
LuisHernandezDev <lcarlosher@gmail.com>
2023-10-26 20:44:41 -0300

    Realizando pruebas de ejercicios de PG con componentes en React

---
+++ frontend/src/components/Mascota.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Mascota extends Component {
+    render() {
+        return (
+            <div>
+        <h1></h1>
+        <img src="" />
+      </div>
+        )
+    }
+}
+export default Mascota;
