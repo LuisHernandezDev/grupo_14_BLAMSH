76f03858daca29a0deb39c1fc90ad7ccf4fa540a
LuisHernandezDev <lcarlosher@gmail.com>
2023-11-05 19:38:51 -0300

    Se eliminan componentes de prueba para iniciar en limpio el dashboard de React del proyecto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,18 +26,7 @@ function App() {
           <Route path='/FrutasVersionFuncion' Component={FrutasVersionFuncion} />
           <Route element={Error404} />
         </Routes>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        
       </header>
     </div>
   )
--- frontend/src/components/Error404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Error404(params) {
-    <h1>404 Not Found</h1>
-}
-
-export default Error404;
