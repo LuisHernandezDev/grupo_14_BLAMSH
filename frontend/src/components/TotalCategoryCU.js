c6bfd0ac307b7380f8e33624bf7b4101435cc911
LuisHernandezDev <lcarlosher@gmail.com>
2023-11-08 18:01:17 -0300

    Se crea componente con el total de productos por cada categoría

--- frontend/src/components/TotalCategoryCU.js
+++ frontend/src/components/TotalCategoryCU.js
@@ -11,19 +11,22 @@ function TotalCategoryCU() {
             const response = await fetch("http://localhost:3011/api/products");
             const data =await response.json()
             const countByCategory = data.countByCategory
-            const categoryProperties = Object.keys(countByCategory)
-            const total = categoryProperties
-            setTotalProductsCU(total)
-            console.log(total);
+            setTotalProductsCU(countByCategory)
+            console.log(countByCategory);
         }
         categoriesCUData();
     }, []);
     
     return(
         <div>
-            <h1>Categorías con el total de productos</h1>
-            {totalProductsCU.length === 0 ? <p>Cargando...</p> : ''}
-           <p> {totalProductsCU}</p>
+            <h1>Ttotal de productos por categorias</h1>
+            {Object.keys(totalProductsCU).length === 0 ? <p>Cargando...</p> : ''}
+           {Object.keys(totalProductsCU).map((categories, i) => {
+            return(
+                <p key={i}>{categories}: {totalProductsCU[categories]}</p>
+            )
+
+           })}
            
         </div>
     )
