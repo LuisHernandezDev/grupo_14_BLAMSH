1a122c146f6bbddeda63f3248ecef90e1cbb9332
lquispe <liliana.quispe@gmail.com>
2023-11-19 13:41:45 -0300

    se agrega descripcion

--- frontend/src/components/LastProductCreate.js
+++ frontend/src/components/LastProductCreate.js
@@ -3,6 +3,7 @@ import { tokens } from "../styles/theme";
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import StatBox from './StatBox';
 
 function LastProductCreate() {
     const [lastProductDetail, setLastProductDetail] = useState([]);
@@ -30,51 +31,112 @@ function LastProductCreate() {
     return (
         <Box
 
-        gridRow="span 2"
-        backgroundColor={colors.primary[400]}
-      >
-        <Box
-          mt="25px"
-          p="0 30px"
-          display="flex "
-          justifyContent="space-between"
-          alignItems="center"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
         >
-          <Box>
-            <Typography
-              variant="h5"
-              fontWeight="600"
-              color={colors.grey[100]}
+            <Box
+                mt="25px"
+                p="0 30px"
+                display="flex "
+                justifyContent="space-between"
+                alignItems="center"
             >
-              Último producto creado
-            </Typography>
-            <Typography
-              variant="h3"
-              fontWeight="bold"
-              color={colors.greenAccent[500]}
-            >
-              {lastProductDetail.name}
-            </Typography>
-          </Box>
-          <Box>
-            <IconButton>
-              <DownloadOutlinedIcon
-                sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
-              />
-            </IconButton>
-          </Box>
-        </Box>
-        <Box height="250px" m="-20px 0 0 0">
-        <Typography
-              variant="h5"
-              fontWeight="600"
-              color={colors.grey[100]}
-            >
-        {lastProductDetail.description}
-             
-            </Typography>
-        </Box>
-      </Box>
+                <Box>
+
+                    <Typography
+                        variant="h3"
+                        fontWeight="bold"
+                        color={colors.greenAccent[500]}
+                    >
+                        Ultimo producto Creado
+                    </Typography>
+                </Box>
+                <Box>
+
+                    <Typography
+                        variant="h3"
+                        fontWeight="bold"
+                        color={colors.greenAccent[500]}
+                    >
+                        {lastProductDetail.name}
+
+                    </Typography>
+                </Box>
+
+
+
+
+            </Box>
+            <Box width="100%" m="0 30px">
+
+                <Box display="flex" justifyContent="space-between" mt="2px">
+                    <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+                        {lastProductDetail.name}
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        fontStyle="italic"
+                        sx={{ color: colors.greenAccent[600] }}
+                    >
+
+                    </Typography>
+                </Box>
+
+
+            </Box>
+            <Box width="100%" m="0 30px">
+
+                <Box display="flex" justifyContent="space-between" mt="2px">
+                    <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+                        {lastProductDetail.description}
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        fontStyle="italic"
+                        sx={{ color: colors.greenAccent[600] }}
+                    >
+
+                    </Typography>
+                </Box>
+
+
+            </Box>
+
+            <Box width="100%" m="0 30px">
+
+                <Box display="flex" justifyContent="space-between" mt="2px">
+                    <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+                        {lastProductDetail.price}
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        fontStyle="italic"
+                        sx={{ color: colors.greenAccent[600] }}
+                    >
+
+                    </Typography>
+                 
+                </Box>
+
+
+            </Box>
+
+            <Box width="100%" m="0 30px">
+
+                <Box display="flex" justifyContent="space-between" mt="2px">
+                   
+                    <img className='img-product-detail' src={lastProductDetail.image} alt="" />
+                </Box>
+
+
+            </Box>
+
+            <Box height="250px" m="-20px 0 0 0">
+
+            </Box>
+        </Box >
+
+
 
     )
 
--- frontend/src/components/TotalCategoryCU.js
+++ frontend/src/components/TotalCategoryCU.js
@@ -17,14 +17,14 @@ function TotalCategoryCU() {
 
     useEffect(() => {
         console.log('Se montó el componente');
-        
+
         async function categoriesCUData() {
-                try {
+            try {
                 const response = await fetch("http://localhost:3011/api/products");
                 const data = await response.json()
-                const countByCategory = data.countByCategory                
+                const countByCategory = data.countByCategory
                 setTotalProductsCU(countByCategory);
-        
+
                 console.log(Object.keys(countByCategory).length);
 
             } catch (error) {
@@ -37,57 +37,51 @@ function TotalCategoryCU() {
     return (
         <Box
 
-        gridRow="span 2"
-        backgroundColor={colors.primary[400]}
-      >
-        <Box
-          mt="25px"
-          p="0 30px"
-          display="flex "
-          justifyContent="space-between"
-          alignItems="center"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
         >
-          <Box>
-            <Typography
-              variant="h5"
-              fontWeight="600"
-              color={colors.grey[100]}
+            <Box
+                mt="25px"
+                p="0 30px"
+                display="flex "
+                justifyContent="space-between"
+                alignItems="center"
             >
-            
-            </Typography>
-            <Typography
-              variant="h3"
-              fontWeight="bold"
-              color={colors.greenAccent[500]}
-            >
-             Lista de categorías
-            </Typography>
-          </Box>
-         
+                <Box>
+                   
+                    <Typography
+                        variant="h3"
+                        fontWeight="bold"
+                        color={colors.greenAccent[500]}
+                    >
+                        Lista de categorías
+                    </Typography>
+                </Box>
+
+            </Box>
+            <Box height="250px" m="-20px 0 0 0">
+                <StatBox
+                    title={Object.keys(totalProductsCU).length === 0 ? <p>Cargando...</p> :
+                        Object.keys(totalProductsCU).map((categories, i) => {
+                            return (
+                                <p key={i}>{categories}: {totalProductsCU[categories]}</p>
+                            )
+
+                        })}
+
+
+                    subtitle="Total de Categorías"
+                    progress="0.80"
+                    increase="+43%"
+
+                />
+
+            </Box>
         </Box>
-        <Box height="250px" m="-20px 0 0 0">
-        <StatBox
-          title= {Object.keys(totalProductsCU).length === 0 ? <p>Cargando...</p> : 
-          Object.keys(totalProductsCU).map((categories, i) => {
-            return (
-                <p key={i}>{categories}: {totalProductsCU[categories]}</p>
-            )
 
-        })}
 
-          
-          subtitle="Total de Categorías"
-          progress="0.80"
-          increase="+43%"
-         
-        />
-       
-        </Box>
-      </Box>
 
-        
-       
-        
+
     )
 
 }
